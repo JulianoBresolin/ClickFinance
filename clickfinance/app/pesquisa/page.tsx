adbc543a81d1e2440dd070e692a52b07bc523ac0
juliano bresolin <bresolinfotografia@gmail.com>
2026-02-16 17:29:40 -0300

    pesquisa

--- clickfinance/app/pesquisa/page.tsx
+++ clickfinance/app/pesquisa/page.tsx
@@ -1,13 +1,12 @@
 export default function Pesquisa() {
 	return (
-		<section className="flex flex-col items-center h-full w-full px-4">
+		<div className="fixed inset-0 pt-20 overflow-hidden bg-white">
 			<iframe
 				src="https://docs.google.com/forms/d/e/1FAIpQLScRJhyLoK72De__Z3u8wwQB15CesJniwPLOJwejin99YYEBbw/viewform?embedded=true"
-				className="w-full max-w-[640px]"
-				height="5005"
+				className="w-full h-full border-0"
 			>
 				Carregando…
 			</iframe>
-		</section>
+		</div>
 	);
 }
